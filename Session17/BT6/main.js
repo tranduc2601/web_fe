496bd3406a748dd53aa05a751f9e477ccb794637
tranduc2601 <breakgamer2601@gmail.com>
2025-03-18 11:49:49 +0700

    fix

--- Session17/BT6/main.js
+++ Session17/BT6/main.js
@@ -1,9 +1,10 @@
+// chuyen thanh so
 console.log("Chuyển đổi thành số:");
-console.log(Number("5"));
-console.log(Number("-1"));
-console.log(Number("Hello"));
-
+console.log(Number("5")); 
+console.log(Number("-1")); 
+console.log(Number("Hello")); 
+// ---> chuyen thanh chuoi
 console.log("Chuyển đổi thành chuỗi:");
 console.log(String(55));
 console.log(String(-10));
-console.log(String(0));
+console.log(String(0)); 
