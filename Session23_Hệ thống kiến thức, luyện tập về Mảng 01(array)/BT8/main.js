2466545f4c6b8ae4177ebd2a498d1d0e39f8f3da
tranduc2601 <breakgamer2601@gmail.com>
2025-03-27 09:46:24 +0700

    fail

--- Session23_Hệ thống kiến thức, luyện tập về Mảng 01(array)/BT8/main.js
+++ Session23_Hệ thống kiến thức, luyện tập về Mảng 01(array)/BT8/main.js
@@ -1,18 +1,24 @@
-const arr = [3, 9, 7, 4, 1, 8, 6, 2, 5, 7];
-const frequency = {};
-for (let i = 0; i < arr.length; i++) {
-    const num = arr[i];
-    frequency[num] = (frequency[num] || 0) + 1;
-}
-
-let maxFrequency = 0;
-let result = Number.MAX_SAFE_INTEGER;
-
-for (const num in frequency) {
-    if (frequency[num] > maxFrequency || (frequency[num] === maxFrequency && +num < result)) {
-        maxFrequency = frequency[num];
-        result = +num;
+let n = parseInt(prompt("Nhập số lượng phần tử của mảng:"));
+if (n < 0) {
+    document.writeln("Số lượng phần tử không được nhỏ hơn 0");
+} else {
+    let arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(parseInt(prompt(`Nhập phần tử thứ ${i + 1}:`)));
+    }
+    if (n === 0) {
+        document.writeln("Không phải dãy số Fibonacci");
+    } else if (n === 1 || n === 2) {
+            document.writeln("Không thể xác định (Cần ít nhất 3 phần tử)");
+    } else {
+        //calc fibo
+        let isFibonacci = true;
+        for (let i = 2; i < n; i++) {
+            if (arr[i] !== arr[i - 1] + arr[i - 2]) {
+                isFibonacci = false;
+                break;
+            }
+        }
+        document.writeln(isFibonacci ? "Là dãy số Fibonacci" : "Không phải dãy số Fibonacci");
     }
 }
-
-document.writeln(result);
